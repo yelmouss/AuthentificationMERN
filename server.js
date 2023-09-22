89031dab8caf6e5b3c012241cc626f19b2a44a84
yelmouss <yassine.elmouss@sfr.fr>
2023-09-22 12:58:13 +0100

    config prod

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
 app.use(express.json());
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
 // Connexion à la base de données MongoDB
@@ -19,13 +19,19 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error(err));
 
-    //header d'accès global à l'API
-    app.use((req, res, next) => {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-      next();
-    });
+//header d'accès global à l'API
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 // Importer les routes d'authentification
 const authRoutes = require("./routes/authRoutes");
 app.use("/api/auth", authRoutes);
@@ -37,17 +43,18 @@ app.use("/api", MailingRoutes);
 const ImagesRoutes = require("./routes/Images");
 app.use("/api", ImagesRoutes);
 
-const path = require('path');
+const path = require("path");
 
 // Serve the static React app (build) from the "client/build" directory
-app.use(express.static(path.join(__dirname, 'front/build')));
-app.use('/images', express.static(path.join(__dirname, 'images')));
+// app.use(express.static(path.join(__dirname, "front/build")));
+app.use("/images", express.static(path.join(__dirname, "images")));
 // Always serve the React app's HTML for all routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'front/build', 'index.html'));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "front/build", "index.html"));
+// });
 
 const port = process.env.PORT || 8000;
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
