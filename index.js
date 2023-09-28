ef3321cc1d513e30346fea306abfe94d83fad406
yelmouss <yassine.elmouss@sfr.fr>
2023-09-28 22:39:20 +0100

    config api vercel

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 require("dotenv").config();
