353e334d4886a37606888eba675cc0e6ee03e8a4
yelmouss <yassine.elmouss@sfr.fr>
2023-09-28 01:25:08 +0100

    IGController

--- controllers/IGController.js
+++ controllers/IGController.js
@@ -1,39 +1,54 @@
 const axios = require("axios");
+require("dotenv").config();
+
+// URL de l'API Instagram
+const instagramApiUrl = "https://graph.instagram.com";
+
+// Noms des champs
+const fields = {
+  id: "id",
+  caption: "caption",
+  data: "data",
+  permalink: "permalink",
+  mediaType: "media_type",
+  mediaUrl: "media_url",
+  thumbnailUrl: "thumbnail_url",
+  username: "username",
+};
 
 exports.getIgPhoto = async (req, res) => {
   try {
     const response = await axios.get(
-      "https://graph.instagram.com/me/media?fields=id,caption,data,permalink,username,media_type,media_url,thumbnail_url&access_token=IGQWRPeVJYRERKRjFhZA09zbm9kbVpwN3dJSWNPSGpNZAnlpYkI3WS02aU5SamZAlSW5Qa0o2N2ZAHR0pxR05fdFdZAUDJTOWZAtWnNTNDBZAbXpjYkFkbFI0cnktcE1hbnFuZAW9ZAUVVXZATNqY2pnSl9KdWFkUWNIOG81Q2cZD"
+      `${instagramApiUrl}/me/media?fields=${Object.values(fields).join(",")}&access_token=${process.env.IG_TOKEN}`
     );
-    //   const filteredPhotos = response.data.data.filter(photo => photo.media_type === 'IMAGE');
     res.json(response.data.data);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      error: "Erreur lors de la récupération des données d'Instagram" + error,
-    });
+    handleInstagramError(res, error);
   }
 };
 
 exports.getIgCount = async (req, res) => {
   try {
     const response = await axios.get(
-      "https://graph.instagram.com/me/media?fields=id,caption,data,permalink,media_type,media_url,username,thumbnail_url&access_token=IGQWRPeVJYRERKRjFhZA09zbm9kbVpwN3dJSWNPSGpNZAnlpYkI3WS02aU5SamZAlSW5Qa0o2N2ZAHR0pxR05fdFdZAUDJTOWZAtWnNTNDBZAbXpjYkFkbFI0cnktcE1hbnFuZAW9ZAUVVXZATNqY2pnSl9KdWFkUWNIOG81Q2cZD"
+      `${instagramApiUrl}/me/media?fields=${Object.values(fields).join(",")}&access_token=${process.env.IG_TOKEN}`
     );
     const photos = response.data.data;
     const photoCount = photos.length;
-    // Filtrer les photos avec des légendes non vides
+    
     const photosWithCaptions = photos.filter(
-      (photo) => photo.caption && photo.caption.length > 0
+      (photo) => photo[fields.caption] && photo[fields.caption].length > 0
     );
 
     const photoIMG = photos.filter(
       (photo) =>
-        photo.media_type === "IMAGE" || photo.media_type === "CAROUSEL_ALBUM"
+        photo[fields.mediaType] === "IMAGE" ||
+        photo[fields.mediaType] === "CAROUSEL_ALBUM"
     );
 
-    const photoVideo = photos.filter((photo) => photo.media_type === "VIDEO");
-    // Obtenir le nombre de photos avec des légendes
+    const photoVideo = photos.filter(
+      (photo) => photo[fields.mediaType] === "VIDEO"
+    );
+
     const photoCountWithCaptions = photosWithCaptions.length;
     res.json({
       count: photoCount,
@@ -42,25 +57,26 @@ exports.getIgCount = async (req, res) => {
       photoVideo: photoVideo.length,
     });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      error: "Erreur lors de la récupération des données d'Instagram" + error,
-    });
+    handleInstagramError(res, error);
   }
 };
 
 exports.getIgFollowers = async (req, res) => {
   try {
     const response = await axios.get(
-      "https://graph.instagram.com/v12.0/me?fields=id,username,account_type,media_count,media&access_token=IGQWRPeVJYRERKRjFhZA09zbm9kbVpwN3dJSWNPSGpNZAnlpYkI3WS02aU5SamZAlSW5Qa0o2N2ZAHR0pxR05fdFdZAUDJTOWZAtWnNTNDBZAbXpjYkFkbFI0cnktcE1hbnFuZAW9ZAUVVXZATNqY2pnSl9KdWFkUWNIOG81Q2cZD"
+      `${instagramApiUrl}/v12.0/me?fields=id,username,account_type,media_count,media,follows_count&access_token=${process.env.IG_TOKEN}`
     );
     const followersCount = response.data;
     res.json({ followersCount });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      error: "Erreur lors de la récupération des données d'Instagram" + error,
-    });
+    handleInstagramError(res, error);
   }
 };
 
+// Fonction de gestion des erreurs Instagram
+function handleInstagramError(res, error) {
+  console.error(error);
+  res.status(500).json({
+    error: "Erreur lors de la récupération des données d'Instagram" + error,
+  });
+}
